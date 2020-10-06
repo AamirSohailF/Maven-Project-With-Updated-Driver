a4b3a045d5002f7443bf430bb7842a770a454ed3
unknown <sochi29@gmail.com>
2020-10-06 13:21:11 +0530

    Adding new Test Case TC009'

---
+++ Skillkart/src/test/java/com/etrading/skillkart/scripts/TC009.java
@@ -0,0 +1,10 @@
+package com.etrading.skillkart.scripts;
+import org.testng.annotations.Test;
+public class TC008 extends BaseTest
+{
+	@Test(description="for github")
+	public void TestCase()
+	{
+		
+	}
+}
