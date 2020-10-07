8c423962ca3d6efcddc84ae62da4924a640f5be6
AamirJordan <aamir0105@gmail.com>
2020-10-08 01:19:39 +0530

    This is My Working Maven Project

---
+++ Skillkart/src/test/java/com/etrading/skillkart/scripts/TC008.java
@@ -0,0 +1,12 @@
+package com.etrading.skillkart.scripts;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC008 extends BaseTest
+{
+	@Test(description="Testing Commit From Eclipse")
+	public void testEclipseCommit()
+	{
+		Reporter.log("Testing Commit From Eclipse");
+	}
+}
